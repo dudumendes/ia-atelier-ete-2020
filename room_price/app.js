3d00722b715b21a4e15c866b9bf874a73d83656a
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2020-07-06 13:50:32 -0400

    Example with price(x) and no. of rooms(y)

---
+++ room_price/app.js
@@ -0,0 +1,34 @@
+console.log('Hi, Tensor Flow');
+
+async function getData() {
+	const houseDdataReq = await fetch('https://raw.githubusercontent.com/meetnandu05/ml1/master/house.json');
+	const houseData = await houseDdataReq.json();
+
+	const cleaned = houseData.map( house => ({
+		price: house.price,
+		rooms: house.AvgAreaNumberofRooms,
+	})).filter(house => (house.price != null && house.rooms != null));
+
+	return cleaned;
+}
+
+async function run() {
+	// Load and plot the original input data we are going to train on
+	const data = await getData();
+	const values = data.map(d => ({
+		x: d.rooms,
+		y: d.price,
+	}))
+
+	tfvis.render.scatterplot(
+		{name: 'No. of rooms vs Price'},
+		{values},
+		{
+			xLabel: 'N.o of Rooms',
+			yLabel: 'Price',
+			height: 300
+		}
+	);
+}
+
+document.addEventListener('DOMContentLoaded', run)
